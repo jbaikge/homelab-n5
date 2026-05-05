029ea6bc7ccada8994da07ae9a839d4dc302e667
jake <523216+jbaikge@users.noreply.github.com>
2026-05-04 20:39:26 -0400

    feat: add it tools and omni tools

---
+++ it-tools.tf
@@ -0,0 +1,33 @@
+resource "docker_image" "it_tools" {
+  provider     = docker.hosts[var.apps.it_tools]
+  name         = "ghcr.io/corentinth/it-tools:2024.10.22-7ca5933"
+  keep_locally = false
+}
+
+resource "docker_container" "it_tools" {
+  provider = docker.hosts[var.apps.it_tools]
+  name     = "it-tools"
+  hostname = "it-tools"
+  image    = docker_image.it_tools.image_id
+  restart  = "unless-stopped"
+
+  labels {
+    label = "traefik.enable"
+    value = "true"
+  }
+
+  labels {
+    label = "traefik.http.routers.it_tools.rule"
+    value = "Host(`it-tools.${data.sops_file.secrets.data["domain.tld"]}`)"
+  }
+
+  labels {
+    label = "traefik.http.routers.it_tools.entrypoints"
+    value = "websecure"
+  }
+
+  labels {
+    label = "traefik.http.services.it_tools.loadbalancer.server.port"
+    value = "80"
+  }
+}
---
+++ omni-tools.tf
@@ -0,0 +1,33 @@
+resource "docker_image" "omni_tools" {
+  provider     = docker.hosts[var.apps.omni_tools]
+  name         = "iib0011/omni-tools:0.6.0"
+  keep_locally = false
+}
+
+resource "docker_container" "omni_tools" {
+  provider = docker.hosts[var.apps.omni_tools]
+  name     = "omni-tools"
+  hostname = "omni-tools"
+  image    = docker_image.omni_tools.image_id
+  restart  = "unless-stopped"
+
+  labels {
+    label = "traefik.enable"
+    value = "true"
+  }
+
+  labels {
+    label = "traefik.http.routers.omni_tools.rule"
+    value = "Host(`omni-tools.${data.sops_file.secrets.data["domain.tld"]}`)"
+  }
+
+  labels {
+    label = "traefik.http.routers.omni_tools.entrypoints"
+    value = "websecure"
+  }
+
+  labels {
+    label = "traefik.http.services.omni_tools.loadbalancer.server.port"
+    value = "80"
+  }
+}
