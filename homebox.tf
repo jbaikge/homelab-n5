7d650402474bd17a49775d9569d4aca88b8d00cc
jake <523216+jbaikge@users.noreply.github.com>
2026-05-06 05:56:38 -0400

    feat: add homebox

---
+++ homebox.tf
@@ -0,0 +1,52 @@
+resource "docker_image" "homebox" {
+  provider     = docker.hosts[var.apps.homebox]
+  name         = "ghcr.io/sysadminsmedia/homebox:0.24.2"
+  keep_locally = false
+}
+
+resource "docker_container" "homebox" {
+  provider = docker.hosts[var.apps.homebox]
+  name     = "homebox"
+  hostname = "homebox"
+  image    = docker_image.homebox.image_id
+  restart  = local.restart
+
+  env = [
+  ]
+
+  labels {
+    label = "traefik.enable"
+    value = "true"
+  }
+
+  labels {
+    label = "traefik.http.routers.homebox.rule"
+    value = "Host(`homebox.${data.sops_file.secrets.data["domain.tld"]}`)"
+  }
+
+  labels {
+    label = "traefik.http.routers.homebox.entrypoints"
+    value = "websecure"
+  }
+
+  labels {
+    label = "traefik.http.services.homebox.loadbalancer.server.port"
+    value = "7745"
+  }
+
+  networks_advanced {
+    name = docker_network.traefik[var.apps.bentopdf].id
+  }
+
+  volumes {
+    container_path = "/etc/localtime"
+    host_path      = "/etc/localtime"
+    read_only      = true
+  }
+
+  volumes {
+    container_path = "/data"
+    host_path      = "/mnt/tank/apps/homebox"
+    read_only      = false
+  }
+}
