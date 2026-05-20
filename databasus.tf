6370757a3d0285758484a2345e746dc8e2ed34d2
jake <523216+jbaikge@users.noreply.github.com>
2026-05-20 12:22:13 -0400

    chore(databasus): upgrade to v3.39.2

--- databasus.tf
+++ databasus.tf
@@ -1,6 +1,6 @@
 resource "docker_image" "databasus" {
   provider     = docker.hosts[var.apps.databasus]
-  name         = "databasus/databasus:v3.35.1"
+  name         = "databasus/databasus:v3.39.2"
   keep_locally = false
 }
 
