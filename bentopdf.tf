49993db05115be1e86dfd0ac685184068c0df87b
jake <523216+jbaikge@users.noreply.github.com>
2026-07-27 07:17:33 -0400

    chore(bentopdf): upgrade to v2.8.7

--- bentopdf.tf
+++ bentopdf.tf
@@ -1,6 +1,6 @@
 resource "docker_image" "bentopdf" {
   provider     = docker.hosts[var.apps.bentopdf]
-  name         = "ghcr.io/alam00000/bentopdf:v2.8.3"
+  name         = "ghcr.io/alam00000/bentopdf:v2.8.7"
   keep_locally = false
 }
 
