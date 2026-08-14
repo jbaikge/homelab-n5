8789ac55007136b0367d1dafa7109d261a6ea521
jake <523216+jbaikge@users.noreply.github.com>
2026-08-14 14:11:57 -0400

    chore(gotenberg): upgrade to v8.36.0

--- gotenberg.tf
+++ gotenberg.tf
@@ -1,6 +1,6 @@
 resource "docker_image" "gotenberg" {
   provider     = docker.hosts[var.apps.gotenberg]
-  name         = "docker.io/gotenberg/gotenberg:8.34.0"
+  name         = "docker.io/gotenberg/gotenberg:8.36.0"
   keep_locally = false
 }
 
