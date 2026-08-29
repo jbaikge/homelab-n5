f11530d873f1bef83070387be6e88aaa9a7fd6c1
jake <523216+jbaikge@users.noreply.github.com>
2026-08-29 06:33:17 -0400

    chore(traefik): upgrade to v3.7.12

--- traefik.tf
+++ traefik.tf
@@ -1,7 +1,7 @@
 resource "docker_image" "traefik" {
   for_each     = toset(var.apps.traefik)
   provider     = docker.hosts[each.key]
-  name         = "traefik:v3.7.9"
+  name         = "traefik:v3.7.12"
   keep_locally = false
 }
 
