0f553d10755c17eedbfe42d5a0dfca49809b31cc
jake <523216+jbaikge@users.noreply.github.com>
2026-08-29 06:32:24 -0400

    chore(paperless-ngx): upgrade to v3.1.0

--- paperless-ngx.tf
+++ paperless-ngx.tf
@@ -1,6 +1,6 @@
 resource "docker_image" "paperless_ngx" {
   provider     = docker.hosts[var.apps.paperless_ngx]
-  name         = "ghcr.io/paperless-ngx/paperless-ngx:3.0.5"
+  name         = "ghcr.io/paperless-ngx/paperless-ngx:3.1.0"
   keep_locally = false
 }
 
