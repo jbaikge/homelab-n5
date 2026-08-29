339d26088d182207788d9703e023ad0e9a37dec1
jake <523216+jbaikge@users.noreply.github.com>
2026-08-29 07:14:41 -0400

    chore(home-assistant): upgrade to v2026.8.3

--- home-assistant.tf
+++ home-assistant.tf
@@ -1,6 +1,6 @@
 resource "docker_image" "home_assistant" {
   provider     = docker.hosts[var.apps.home_assistant]
-  name         = "ghcr.io/home-assistant/home-assistant:2026.7.1"
+  name         = "ghcr.io/home-assistant/home-assistant:2026.8.3"
   keep_locally = false
 }
 
