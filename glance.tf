8feeee4065aad9f3ae94da240a42ac44467d8902
jake <523216+jbaikge@users.noreply.github.com>
2026-09-03 17:55:03 -0400

    chore(glance): upgrade to v0.8.6

--- glance.tf
+++ glance.tf
@@ -1,6 +1,6 @@
 resource "docker_image" "glance" {
   provider     = docker.hosts[var.apps.glance]
-  name         = "glanceapp/glance:v0.8.5"
+  name         = "glanceapp/glance:v0.8.6"
   keep_locally = false
 }
 
