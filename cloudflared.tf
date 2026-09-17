7592c98cc3adc03e263398a4b486b0cbfc2de28f
jake <523216+jbaikge@users.noreply.github.com>
2026-09-17 17:54:01 -0400

    chore(cloudflared): upgrade to v2026.9.1

--- cloudflared.tf
+++ cloudflared.tf
@@ -1,7 +1,7 @@
 resource "docker_image" "cloudflared" {
   for_each     = toset(var.apps.cloudflared)
   provider     = docker.hosts[each.key]
-  name         = "cloudflare/cloudflared:2026.7.3"
+  name         = "cloudflare/cloudflared:2026.9.1"
   keep_locally = false
 }
 
